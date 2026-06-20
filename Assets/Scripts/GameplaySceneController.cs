fd6ba1498e3e3a6059aae222b83f690a333f574d
Matthew Hoover <61717155+mrhooverUCSC@users.noreply.github.com>
2026-06-20 00:16:10 -0700

    AdditiveScene Structure
    CombatScene additively loads in the additive scenes it relies on

---
+++ Assets/Scripts/GameplaySceneController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameplaySceneController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SceneManager.LoadScene("CombatAdditive", LoadSceneMode.Additive);
+        SceneManager.LoadScene("GearAdditive", LoadSceneMode.Additive);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
