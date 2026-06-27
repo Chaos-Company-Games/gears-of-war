7f63e7e634cf65b24c09dbf38bed47f0f40e5ad8
Matthew Hoover <61717155+mrhooverUCSC@users.noreply.github.com>
2026-06-26 20:49:41 -0700

    change ability socket size

--- Assets/Scripts/AdaptiveGearController.cs
+++ Assets/Scripts/AdaptiveGearController.cs
@@ -113,7 +113,7 @@ public class AdaptiveGearController : MonoBehaviour
             if (hasAbilitySlots && abilitySlots.Contains(i)) //this is why this list exists, alongside the abilitySocketControllers list. it's way more indexable, makes it easy to tell if there's a slot that's supposed to be here.
             {
                 AbilitySocketController socketController = abilitySocketControllers.Find(x => x.tooth == i); //find the abilitySocketController for this tooth
-                socketController.GetComponent<RectTransform>().anchoredPosition = (spawnDir * radius * pitch * (0.6f * (.1f * (float)teeth))); //place it slightly behind the tooth
+                socketController.GetComponent<RectTransform>().anchoredPosition = (spawnDir * radius * pitch * 0.6f); //place it slightly behind the tooth
             }
         }
     }
